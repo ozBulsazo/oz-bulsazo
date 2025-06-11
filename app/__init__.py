34cc66233af0ba95292609be66ce7c2bfc09c5d2
Dayeon <0812dayeon@gmail.com>
2025-06-11 16:52:07 +0900

    mission1 초기설정 완료

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+from flask_migrate import Migrate
+
+import app.models
+from app.routes import register_routes
+from config import db
+
+migrate = Migrate()
+
+
+def create_app():
+    application = Flask(__name__)
+
+    application.config.from_object("config.Config")
+    application.secret_key = "oz_form_secret"
+
+    db.init_app(application)
+
+    migrate.init_app(application, db)
+
+		# 400 에러 발생 시, JSON 형태로 응답 반환
+    @application.errorhandler(400)
+    def handle_bad_request(error):
+        response = jsonify({"message": error.description})
+        response.status_code = 400
+        return response
+
+		# app/route/__init__.py에 블루 브린트를 등록해주세요
+	register_routes(application)
+
+
+    return application
